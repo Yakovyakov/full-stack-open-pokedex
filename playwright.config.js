8dde9bb638a9f22894ef76b8a9f62b3e1f307222
Yakovyakov <yyak0423@gmail.com>
2025-02-11 15:33:38 -0500

    fix exercise 11.9

--- playwright.config.js
+++ playwright.config.js
@@ -8,6 +8,7 @@ const { defineConfig, devices } = require('@playwright/test')
  */
 module.exports = defineConfig({
   testDir: './e2e-tests',
+  testIgnore: '*example.spec.js',
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
